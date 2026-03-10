2290c80525df789cd82698b3ac9f66c0338483df
Pramana21 <pramanaorion2171@gmail.com>
2026-03-10 16:00:18 +0800

    Complete Pegawai CRUD (create read update delete)

--- resources/views/pegawai/index.blade.php
+++ resources/views/pegawai/index.blade.php
@@ -5,20 +5,40 @@
 </a>
 
 <table border="1">
-    <tr>
-        <th>NIP</th>
-        <th>Nama</th>
-        <th>Jabatan</th>
-        <th>Unit Kerja</th>
-    </tr>
+<tr>
+<th>NIP</th>
+<th>Nama</th>
+<th>Jabatan</th>
+<th>Unit Kerja</th>
+<th>Aksi</th>
+</tr>
 
-    @foreach ($pegawai as $p)
-    <tr>
-        <td>{{ $p->nip }}</td>
-        <td>{{ $p->nama }}</td>
-        <td>{{ $p->jabatan }}</td>
-        <td>{{ $p->unit_kerja }}</td>
-    </tr>
-    @endforeach
+@foreach ($pegawai as $p)
+<tr>
+
+<td>{{ $p->nip }}</td>
+<td>{{ $p->nama }}</td>
+<td>{{ $p->jabatan }}</td>
+<td>{{ $p->unit_kerja }}</td>
+
+<td>
+
+<a href="{{ route('pegawai.edit', $p->id) }}">
+<button>Edit</button>
+</a>
+
+<form action="{{ route('pegawai.destroy', $p->id) }}" method="POST" style="display:inline">
+
+@csrf
+@method('DELETE')
+
+<button type="submit">Delete</button>
+
+</form>
+
+</td>
+
+</tr>
+@endforeach
 
 </table>
