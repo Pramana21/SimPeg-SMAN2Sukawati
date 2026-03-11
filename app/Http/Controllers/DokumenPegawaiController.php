897daae828a16b439b2d263ce1772a7292fbbfa2
Pramana21 <pramanaorion2171@gmail.com>
2026-03-11 16:05:01 +0800

    Menambahkan validasi upload dokumen pegawai untuk memastikan file yang diupload sesuai format dan ukuran

--- app/Http/Controllers/DokumenPegawaiController.php
+++ app/Http/Controllers/DokumenPegawaiController.php
@@ -3,8 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Pegawai;
+use App\Models\DokumenPegawai;
 
 class DokumenPegawaiController extends Controller
 {
-    //
+
+    public function create()
+    {
+        $pegawai = Pegawai::all();
+
+        return view('dokumen.create', compact('pegawai'));
+    }
+
+    public function store(Request $request)
+{
+
+    $request->validate([
+        'pegawai_id' => 'required',
+        'nama_dokumen' => 'required',
+        'file_dokumen' => 'required|file|mimes:pdf,jpg,png|max:2048'
+    ]);
+
+    $file = $request->file('file_dokumen');
+
+    $namaFile = time().'_'.$file->getClientOriginalName();
+
+    $file->move(public_path('dokumen'), $namaFile);
+
+    DokumenPegawai::create([
+        'pegawai_id' => $request->pegawai_id,
+        'nama_dokumen' => $request->nama_dokumen,
+        'file_dokumen' => $namaFile
+    ]);
+
+    return redirect('/dashboard');
+
+}
+
 }
--- app/Models/Pegawai.php
+++ app/Models/Pegawai.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Pegawai extends Model
 {
     protected $table = 'pegawai';
-
+    
     protected $fillable = [
         'nip',
         'nama',
